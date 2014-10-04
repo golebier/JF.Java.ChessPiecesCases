dae616a5f4f6f85c6aabcbe806dd4a1ba0debd51
Gra <golebier@gmail.com>
2014-10-04 10:31:49 +0200

    Reading input test, base.

---
+++ src/test/java/exercise/cpc/data/input/InputReadTest.java
@@ -0,0 +1,28 @@
+package exercise.cpc.data.input;
+//
+//Copyright (c) 1979, the Gra projects.
+//Please see the AUTHORS file for details.
+//All rights reserved.
+//Use of this source code is governed by a MIT-style license
+//that can be found in the LICENSE file.
+//
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+/**
+ *  TOOD correct doc.
+ *  Basic test for reading input data.  
+ *  Should test:
+ *   the board dimensions reading and saving for later
+ *   each type pieces with amount
+ *
+ * @author Gra <Gołębiewski Radosław A.>
+ *      https://github.com/golebier or https://golebier.github.io
+ *      https://google.com/+RadoslawGolebiewski
+ *      http://www.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35
+*/
+public class InputReadTest {
+    @Test
+    public void shouldRunFullTest() throws IOException {
+    }
+}
